d0b4c1fb158dd518da8212a8767b4d7ee7c4760f
dennohpeter <denispeterson96@gmail.com>
2019-03-01 15:45:24 +0300

    Changed db to postgresql

--- iCrawl/settings.py
+++ iCrawl/settings.py
@@ -70,18 +70,18 @@ WSGI_APPLICATION = 'iCrawl.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'games_db',
-    #     'USER': 'peterson',
-    #     'PASSWORD': DATABASE_PASSWORD,
-    #     'HOST': 'localhost',
-    #     'PORT': '',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'games_db',
+        'USER': 'peterson',
+        'PASSWORD': DATABASE_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 
