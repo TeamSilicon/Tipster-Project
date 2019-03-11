6c01c86bbd121a764409426c6931b748c3c5449e
Dennis Peterson <denispeterson96@gmail.com>
2019-03-11 11:54:44 +0300

    fixed match start issue #EAT

--- home/statArena.py
+++ home/statArena.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from itertools import chain
 from home.results_overall import overall_result
 import re
+from datetime import timedelta
+import datetime
 
 def stat_arena(page, match_date):
     games_store = []
@@ -19,6 +21,10 @@ def stat_arena(page, match_date):
         for each in games:
             b = list(chain(*get_algo.findall(each.getText())))
             b[0:0] = [match_date]  # inserting date
+            game_time = datetime.datetime.strptime(b[1:1].strip(), "%H:%M")
+            game_time = game_time + timedelta(hours=2)  # updating time to EAT
+            formatted_time = game_time.strftime("%H:%M")
+            b[1:1] = [formatted_time]
             results =  b[3].split(":")
             if len(results) == 2:
                 result_home = int(results[0])
