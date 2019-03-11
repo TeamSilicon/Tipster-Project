aa28dd181264816b46d3ec82b1ff6e3d4e55417d
Dennis Peterson <denispeterson96@gmail.com>
2019-03-11 16:17:03 +0300

    Upgraded toArena APi

--- home/statArena.py
+++ home/statArena.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 from itertools import chain
 from home.results_overall import overall_result
-import re
 from datetime import timedelta
 import datetime
 
@@ -11,32 +10,32 @@ def stat_arena(page, match_date):
     if type(arena) == BeautifulSoup:
         games = arena.findAll(class_="cmatch")
         games_num = len(games)
-        get_algo = re.compile(
-        r'''(
-        \d+:\d+)\s+   # time
-        (12|1X|1|2|X2|X)* # tip
-        (\s*\d+\s*:\d+\s*)*     # outcome
-        (.*)   # team names
-        ''', re.VERBOSE)
         for each in games:
-            b = list(chain(*get_algo.findall(each.getText())))
-            b[0:0] = [match_date]  # inserting date
-            game_time = datetime.datetime.strptime(b[1].strip(), "%H:%M")
+            game_time = each.find(class_="time").getText().strip()
+            game_time = datetime.datetime.strptime(game_time, "%H:%M")
             game_time = game_time + timedelta(hours=2)  # updating time to EAT
-            b[1] = game_time.strftime("%H:%M")
-            results =  b[3].split(":")
-            if len(results) == 2:
-                result_home = int(results[0])
-                result_away = int(results[1])
+            game_time = game_time.strftime("%H:%M")
+            tip = each.find(class_="tip").getText()
+            teams = each.find(class_="teams").getText()
+            print(teams)
+            try :
+                score = each.find(class_="result").getText().strip()
+            except AttributeError:
+                score = "none"
+            results = score.split(":")
+            if len(results) == 2 and score !="none":
+                try:
+                    result_home = int(results[0])
+                    result_away = int(results[1])
+                except ValueError:
+                    result_home = 'no_result'
+                    result_away = 'no_result'
             else:
                 result_home = 'no_result'
                 result_away = 'no_result'
-            tip=b[2]
             tipx_odd=tip1_odd=tip2_odd="0:0"  # making odd zero by default
             outcome, tip_odd = overall_result(result_home, result_away, tip, tipx_odd,tip1_odd,tip2_odd)
-            b[5:5] = [tip_odd]  # inserting tip_odd # 0.0
-            b[6:6] = [outcome]  # determining which team which won
-            games_store.append(b)
+            games_store.append([match_date, game_time, tip, score, teams, tip_odd, outcome])
         return games_store
     else:
         return "not bs4 obj"
