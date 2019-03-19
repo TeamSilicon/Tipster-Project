9345b7a206e5b204e388598d516d044b017505de
Dennis Peterson <denispeterson96@gmail.com>
2019-03-19 19:35:26 +0300

    fixed a bug in urls

--- home/urls.py
+++ home/urls.py
@@ -2,21 +2,31 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # for AllGames
     url(r'^$', views.all_games, name='today'),
     url(r'^yesterday/$', views.all_games, name='yesterday'),
     url(r'^tomorrow/$', views.all_games, name='tomorrow'),
     # url(r'game_detail/(?P<pk>[{\w+}* -{\w+}*]+)', views.game_detail, name="game_detail"),
     url(r'^login/$', views.login, name='login'),
+    # for goalgoal
     url(r'^goalgoal/today/$', views.goal_Goal, name='goalgoal'),
     url(r'^goalgoal/yesterday/$', views.goal_Goal, name='goalgoal'),
     url(r'^goalgoal/tomorrow/$', views.goal_Goal, name='goalgoal'),
     url(r'^goalgoal/$', views.goal_Goal, name='goalgoal'),
+    # for featured
+    url(r'^featured/today/$', views.featured, name='featured'),
+    url(r'^featured/yesterday/$', views.featured, name='featured'),
+    url(r'^featured/tomorrow/$', views.featured, name='featured'),
     url(r'^featured/$', views.featured, name='featured'),
+    # for jackpot
     url(r'^jackpot/$', views.jackpot, name='jackpot'),
-    url(r'^over/today/$', views.goal_Goal, name='over'),
-    url(r'^over/yesterday/$', views.goal_Goal, name='over'),
-    url(r'^over/tomorrow/$', views.goal_Goal, name='over'),
+    # for overtips
+    url(r'^over/today/$', views.over, name='over'),
+    url(r'^over/yesterday/$', views.over, name='over'),
+    url(r'^over/tomorrow/$', views.over, name='over'),
     url(r'^over/$', views.over, name='over'),
+    # for betslip
     url(r'^betslip/', views.slip, name='betslip'),
+    # for comingsoon
     url(r'^comingsoon/', views.comingsoon, name='comingsoon'),
 ]
