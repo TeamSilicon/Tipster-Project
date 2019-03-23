3371ceb89fb813345cbe4ae75c56050e60192cf7
Dennis Peterson <denispeterson96@gmail.com>
2019-03-23 22:46:15 +0300

    debug now False by default

--- iCrawl/settings.py
+++ iCrawl/settings.py
@@ -17,7 +17,7 @@ DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com", 'localhost']
 
