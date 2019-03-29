a7cf3255c2a09efad7fd2b3711663c2441785614
Dennis Peterson <denispeterson96@gmail.com>
2019-03-29 10:34:39 +0300

    reduced yesterday's fetch interval to 3 minutes

--- home/management/commands/updater.py
+++ home/management/commands/updater.py
@@ -26,7 +26,7 @@ class Command(BaseCommand):
             page_content3 = requester(page_urls[2], 3)
             boiler(page_content1, page_content2, page_content3, today)
 
-        @sched.scheduled_job('interval', hours=3)
+        @sched.scheduled_job('interval', minutes=3)
         def update_games_yest():
             today = topnavselector() + timedelta(days=-1)
             zulu_page = 'http://www.zulubet.com/tips-%d-%d-%d.html' % (today.day, today.month, today.year)
