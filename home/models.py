4ffcb0d188816d951161c3b26fa890cd74a11cd7
Dennis Peterson <denispeterson96@gmail.com>
2019-11-06 15:16:50 +0300

    updated database models

--- home/models.py
+++ home/models.py
@@ -3,83 +3,60 @@ from django.utils import timezone
 
 
 class AllGames(models.Model):
-    match_date = models.CharField(max_length=40)
-    time = models.CharField(max_length=40)
+    date = models.CharField(max_length=40)
+    start_time = models.CharField(max_length=40)
     teams = models.CharField(max_length=400, primary_key=True)
-    tip = models.CharField(max_length=10)
-    tip_odd = models.CharField(max_length=10, default="0.00")
-    ft_results = models.CharField(max_length=40)
-    outcome_text = models.CharField(max_length=40)
+    pick = models.CharField(max_length=10)
+    odds = models.CharField(max_length=10, default="0.00")
+    score = models.CharField(max_length=40)
+    won_or_lost = models.CharField(max_length=40)
+
 
 class TipGG(models.Model):
-    match_date = models.CharField(max_length=40)
-    time = models.CharField(max_length=40)
+    date = models.CharField(max_length=40)
+    start_time = models.CharField(max_length=40)
     teams = models.CharField(max_length=400, primary_key=True)
     tip_gg = models.CharField(max_length=10)
-    tip_gg_odd = models.CharField(max_length=10, default="0.00")
-    ft_results = models.CharField(max_length=40)
-    outcome_text = models.CharField(max_length=40)
+    tip_gg_odds= models.CharField(max_length=10, default="0.00")
+    score = models.CharField(max_length=40)
+    won_or_lost = models.CharField(max_length=40)
+
 
 class Over15(models.Model):
-    match_date = models.CharField(max_length=40)
-    time = models.CharField(max_length=40)
+    date = models.CharField(max_length=40)
+    start_time = models.CharField(max_length=40)
     teams = models.CharField(max_length=400, primary_key=True)
     tip_ov = models.CharField(max_length=10)
-    tip_ov_odd = models.CharField(max_length=10, default="0.00")
-    ft_results = models.CharField(max_length=40)
-    outcome_text = models.CharField(max_length=40)
+    tip_ov_odds= models.CharField(max_length=10, default="0.00")
+    score = models.CharField(max_length=40)
+    won_or_lost = models.CharField(max_length=40)
+
 
 class Over25(models.Model):
-    match_date = models.CharField(max_length=40)
-    time = models.CharField(max_length=40)
+    date = models.CharField(max_length=40)
+    start_time = models.CharField(max_length=40)
     teams = models.CharField(max_length=400, primary_key=True)
     tip_ov = models.CharField(max_length=10)
-    tip_ov_odd = models.CharField(max_length=10, default="0.00")
-    ft_results = models.CharField(max_length=40)
-    outcome_text = models.CharField(max_length=40)
+    tip_ov_odds= models.CharField(max_length=10, default="0.00")
+    score = models.CharField(max_length=40)
+    won_or_lost = models.CharField(max_length=40)
+
 
 class Over35(models.Model):
-    match_date = models.CharField(max_length=40)
-    time = models.CharField(max_length=40)
+    date = models.CharField(max_length=40)
+    start_time = models.CharField(max_length=40)
     teams = models.CharField(max_length=400, primary_key=True)
     tip_ov = models.CharField(max_length=10)
-    tip_ov_odd = models.CharField(max_length=10, default="0.00")
-    ft_results = models.CharField(max_length=40)
-    outcome_text = models.CharField(max_length=40)
+    tip_ov_odds= models.CharField(max_length=10, default="0.00")
+    score = models.CharField(max_length=40)
+    won_or_lost = models.CharField(max_length=40)
+
 
 class Featured(models.Model):
-    match_date = models.CharField(max_length=40)
-    time = models.CharField(max_length=40)
+    date = models.CharField(max_length=40)
+    start_time = models.CharField(max_length=40)
     teams = models.CharField(max_length=400, primary_key=True)
-    tip = models.CharField(max_length=10)
-    tip_odd = models.CharField(max_length=10, default="0.00")
-    ft_results = models.CharField(max_length=40)
-    outcome_text = models.CharField(max_length=40)
-
-# class Progress(models.Model):
-#     from_date = models.DateTimeField(
-#         default=timezone.now)
-#     upto_date = models.DateTimeField(
-#         blank=True, null=True)
-#     counter_lost_odd = models.CharField(max_length=20)
-#     counter_won_odd = models.CharField(max_length=20)
-#     counter_lose = models.CharField(max_length=20)
-#     counter_win = models.CharField(max_length=20)
-#
-#     def __str__(self):
-#         return self.from_date
-#
-#     def __str__(self):
-#         return self.upto_date
-#
-#     def __str__(self):
-#         return self.counter_lost_odd
-#
-#     def __str__(self):
-#         return self.counter_win
-#
-#     def __str__(self):
-#         return self.counter_lose
-#
-#     def __str__(self):
-#         return self.counter_won_odd
+    pick = models.CharField(max_length=10)
+    odds = models.CharField(max_length=10, default="0.00")
+    score = models.CharField(max_length=40)
+    won_or_lost = models.CharField(max_length=40)
