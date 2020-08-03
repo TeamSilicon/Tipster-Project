de7d97d36c38a116190e2c887c0afde80d6d532a
Dennis Peterson <denispeterson96@gmail.com>
2020-08-03 14:50:52 +0300

    added sentry

---
+++ iCrawl/settings/prod.py
@@ -0,0 +1,31 @@
+from sentry_sdk.integrations.django import DjangoIntegration
+import sentry_sdk
+from iCrawl.settings.base import *
+import os
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+# For extra security --run python manage.py check --deploy
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+
+
+ALLOWED_HOSTS = [".herokuapp.com"]
+
+
+# SENTRY SETUP
+SENTRY_KEY = os.getenv('SENTRY_KEY')
+SENTRY_ORG = os.getenv('SENTRY_ORG')
+SENTRY_PROJECT = os.getenv('SENTRY_PROJECT')
+
+sentry_sdk.init(
+    dsn=f"https://{SENTRY_KEY}@{SENTRY_ORG}.ingest.sentry.io/{SENTRY_PROJECT}",
+    integrations=[DjangoIntegration()],
+
+    # To associate users to errors
+    send_default_pii=True
+)
